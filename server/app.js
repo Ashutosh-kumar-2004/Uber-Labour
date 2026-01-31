1716308d3a265df13b83264347f1eef26dad04fb
Ashutosh <ashutoshyadav202004@gmail.com>
2026-02-01 00:00:34 +0530

    added the config to connect to mongodb

--- server/app.js
+++ server/app.js
@@ -2,9 +2,10 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js"
+import connectDB from "./config/db.js";
 
 const app = express();
-
+connectDB
 app.use(
     cors({
         origin: "https://localhost:5173",
