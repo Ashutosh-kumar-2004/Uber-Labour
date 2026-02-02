c822e483dd7a41fd8a1f0b16d0ce3e6aa61538a4
Ashutosh <ashutoshyadav202004@gmail.com>
2026-02-02 22:08:50 +0530

    done

--- client/src/appRoutes/AppRoutes.jsx
+++ client/src/appRoutes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignupPage from "../components/Auth/Signup";
 import LoginPage from "../components/Auth/LoginPage";
 import ForgotPassword from "../components/Auth/ForgotPassword";
+import User_Dashboard from "../components/Auth/User_Dashboard";
 
 const AppRoutes = () => {
   return (
@@ -10,6 +11,7 @@ const AppRoutes = () => {
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/User_dashboard" element={<User_Dashboard/>}/>
 
         {/* Default Route */}
         <Route path="*" element={<LoginPage />} />
